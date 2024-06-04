1e671868647b6109e161ec27bf4faef79f1bdb68
Martin Valetta <martin.valetta.50@gmail.com>
2024-06-04 10:44:04 -0300

    nuevo metodo de obt. de tema y preguntas, sist.

--- js/clases.js
+++ js/clases.js
@@ -1,18 +1,106 @@
 class sistema {
+
 	constructor() {
 		this.listaTemas = [];
 		this.listaPreguntas = [];
 	}
+
+	hayTemas() {
+		return this.listaTemas.length = 0
+	}
+
+	hayPreguntas() {
+		return this.listaPreguntas.length = 0
+	}
+
+	agregarTema(useNombre, useDescripcion) {
+
+		let repetido = false;
+		let vacio = false;
+
+		if ((useNombre.length == 0) || (useDescripcion.length == 0)) {
+			vacio = true;
+		}
+
+		for (i=0 ; (i<this.listaTemas.length) && (repetido == false) && (vacio == false) ; i++) {
+			if (this.listaTemas[i].nombre == useNombre) {
+				repetido = true;
+			}
+		}
+
+		if (vacio == true) {
+			alert ("¡No se puede añadir el tema! Alguno de los valores ingresados es nulo.");
+		} else if (repetido == true) {
+			alert ("¡No se puede añadir el tema! Ya existe un tema con este nombre.");
+		} else {
+			let nuevoTema = new tema(useNombre, useDescripcion);
+			this.listaTemas.push(nuevoTema);
+		}
+	}
+
+	agregarPregunta(useTexto, useRespuestaC, useRespuestaI, useNivel, useTema) {
+
+		let repetido = false;
+		let vacio = false;
+		let valido = true;
+		let nivel = true;
+
+		if (useRespuestaI.includes(useRespuestaC)) {
+			valido = false;
+		}
+
+		if ((useTexto.length == 0) || (useRespuestaC.length == 0) || (useRespuestaI.length == 0) || (useNivel.toString().length == 0) || (useTema.length == 0)) {
+			vacio = true;
+		}
+
+		if ((useNivel < 1 ) || (useNivel > 5)) {
+			nivel = false;
+		}
+
+		for (i=0 ; (i<this.listaPreguntas.length) && (repetido == false) && (valido == true) && (vacio == false); i++) {
+			if (this.listaPreguntas[i].texto == useTexto) {
+				repetido = true;
+			}
+		}
+
+		if (valido == false) {
+			alert ("¡No se puede añadir la pregunta! La respuesta correcta esta tambien entre las incorrectas.")
+		} else if(vacio == true) {
+			alert ("¡No se puede añadir la pregunta! No se ha ingresado uno de los datos requeridos.")
+		} else if(repetido == true) {
+			alert ("¡No se puede añadir la pregunta! Ya existe una pregunta con este texto.")
+		} else if(nivel == false) {
+			alert ("¡No se puede añadir la pregunta! El nivel ingresado debe ser entre 1 y 5.")
+		} else {
+			let nuevaPregunta = new pregunta(useTexto, useRespuestaC, useRespuestaI, useNivel, useTema);
+			this.listaPreguntas.push(nuevaPregunta);
+		}
+
+	}
+
+	sortPreguntasTemaCrNivelCr() {}
+
+	sortPreguntasTemaDeNivelCr() {}
+
 }
 
+
 class tema {
+
 	constructor(nombre, descripcion) {
 		this.nombre = nombre;
 		this.descripcion = descripcion;
 	}
+
+	toString() {
+		return "Nombre: " + this.nombre + " Descripcion: " + this.descripcion
+	}
+
 }
 
+
 class pregunta {
+
 	constructor(texto, respuestaCorrecta, respuestasIncorrectas, nivel, tema) {
 		this.texto = texto;
 		this.respuestaCorrecta = respuestaCorrecta;
@@ -20,4 +108,9 @@ class pregunta {
 		this.nivel = nivel;
 		this.tema = tema;
 	}
+
+	toString() {
+		return "Texto: " + this.texto + " Respuesta Correcta: " + this.respuestaCorrecta + " Respuestas Incorrectas: " + this.respuestasIncorrectas + " Nivel: " + this.nivel + " Tema: " + this.tema
+	}
+
 }
--- js/funciones.js
+++ js/funciones.js
@@ -11,11 +11,6 @@ function innit() {
 	info.addEventListener("click", () => divManager(info));
 	admin.addEventListener("click", () => divManager(admin));
 	jugar.addEventListener("click", () => divManager(jugar));
-
-	document.getElementById("agregarTema").addEventListener("click", nuevoTema);
-	document
-		.getElementById("agregarPreg")
-		.addEventListener("click", nuevaPregunta);
 }
 
 function deseaCargarDatos() {
@@ -58,71 +53,4 @@ function divManager(selectedButton) {
 			document.getElementById("jugar").style.display = "block";
 			break;
 	}
-}
-
-function nuevoTema() {
-	let descripcion = document.getElementById("temaDesc").value;
-	let nombre = document.getElementById("temaNombre").value;
-
-	if (MiSistema.listaTemas.some((tema) => tema.nombre == nombre)) {
-		alert("¡Este tema ya existe!");
-	} else {
-		añadirTema(nombre, descripcion);
-	}
-}
-
-function añadirTema(nombreUsar, descripcionUsar) {
-	let nuevoTema = new tema(nombreUsar, descripcionUsar);
-	MiSistema.listaTemas.push(nuevoTema);
-
-	let newOption = document.createElement("option");
-	let newLi = document.createElement("li");
-
-	newLi.innerText = nombreUsar;
-	newOption.innerText = nombreUsar;
-
-	document.getElementById("pregTema").appendChild(newOption);
-	document.getElementById("listaTemas").appendChild(newLi);
-	document.getElementById("altaTemas").reset();
-}
-
-function nuevaPregunta() {
-	let tema = document.getElementById("pregTema").value;
-	let nivel = document.getElementById("pregNivel").value;
-	let texto = document.getElementById("pregText").value;
-	let correcto = document.getElementById("pregResp").value;
-	let incorrecto = document.getElementById("pregIncorr").value;
-
-	if (MiSistema.listaPreguntas.some((tema) => tema.texto == texto)) {
-		alert("¡Esta pregunta ya existe!");
-	} else {
-		añadirPregunta(tema, nivel, texto, correcto, incorrecto);
-	}
-}
-
-function añadirPregunta(tema, nivel, texto, correcto, incorrecto) {
-	let nuevaPregunta = new pregunta(tema, nivel, texto, correcto, incorrecto);
-	MiSistema.listaPreguntas.push(nuevaPregunta);
-
-	let newTr = document.createElement("tr");
-	let newTemaTd = document.createElement("td");
-	let newNivelTd = document.createElement("td");
-	let newTextoTd = document.createElement("td");
-	let newCorrectoTd = document.createElement("td");
-	let newIncorrectoTd = document.createElement("td");
-
-	newTemaTd.innerText = tema;
-	newNivelTd.innerText = nivel;
-	newTextoTd.innerText = texto;
-	newCorrectoTd.innerText = correcto;
-	newIncorrectoTd.innerText = incorrecto;
-
-	newTr.appendChild(newTemaTd);
-	newTr.appendChild(newNivelTd);
-	newTr.appendChild(newTextoTd);
-	newTr.appendChild(newCorrectoTd);
-	newTr.appendChild(newIncorrectoTd);
-
-	document.getElementById("tablaPreguntas").appendChild(newTr);
-	document.getElementById("altaPreg").reset();
 }
